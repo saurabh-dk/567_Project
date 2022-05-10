5bb12088a8dd86c1755dbe12784cf3d8e511e068
Saurabh Kulkarni <srbhkulkarni13@gmail.com>
2022-05-10 02:01:40 -0700

    Checking out api

---
+++ lib/controller/room_connection.dart
@@ -0,0 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class RoomConnection {
+  final db = FirebaseFirestore.instance;
+
+  Future<String?> createRoom({required String code}) async {
+    // db.co
+  }
+}
